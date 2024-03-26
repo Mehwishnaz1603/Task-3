a569c5783a71721f2cbfea3861457b41a3fc099e
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-03-26 13:48:11 +0500

    .

--- index.ts
+++ index.ts
@@ -1,3 +1,8 @@
+/////Task # 3 ////
+
+//Name Cases: Store a person’s name in a variable, and then print that 
+//person’s name in lowercase, uppercase, and titlecase.
+
 let personName: string = "Mehwish Naz";
 
 // in lowercase
